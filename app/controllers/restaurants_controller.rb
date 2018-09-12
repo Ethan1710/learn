cd88245eeda1d41382c695e9fc57ffeb6575aeaa
Ethan Fraenkel <ethan.fraenkel@hotmail.com>
2018-09-12 04:17:37 +0100

    finished crud

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -7,7 +7,40 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.find(params[:id])
   end
 
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @Restaurant = Restaurant.new(restaurant_params)
+    @Restaurant.save
+    redirect_to restaurants_path
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    if @restaurant.update(restaurant_params)
+      redirect_to restaurants_path(@restaurant)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    redirect_to restaurants_path
+  end
+
 
   private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category)
+  end
 
 end
