fc97a7fd8e4ad7801b178d5ac7276839c0325c33
Ethan Fraenkel <ethan.fraenkel@hotmail.com>
2018-09-12 20:06:28 -0400

    done with adding reviews to restaurant controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,25 @@
 class ReviewsController < ApplicationController
+  def new
+    @review = Review.new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.restaurant = Restaurant.find(params[:restaurant_id])
+    @review.save
+    redirect_to restaurant_path(Restaurant.find(params[:restaurant_id]))
+  end
+
+  def destroy
+    @review = review.find(params[:id])
+    @review.destroy
+    redirect_to restaurants_path
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:star, :description, :restaurant_id)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_10_074533) do
+ActiveRecord::Schema.define(version: 2018_09_12_232142) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,9 @@ ActiveRecord::Schema.define(version: 2018_09_10_074533) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "restaurant_id"
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
   end
 
+  add_foreign_key "reviews", "restaurants"
 end
